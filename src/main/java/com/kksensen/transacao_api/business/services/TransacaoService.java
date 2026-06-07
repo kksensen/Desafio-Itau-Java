9960e168905ae1125a24874cafad74cbb37b0871
kksensen <kksensentuts@gmail.com>
2026-06-06 21:00:20 -0300

    Initial commit

---
+++ src/main/java/com/kksensen/transacao_api/business/services/TransacaoService.java
@@ -0,0 +1,11 @@
+package com.kksensen.transacao_api.business.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TransacaoService {
+
+
+}
